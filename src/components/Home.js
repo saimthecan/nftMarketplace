9f71c8fb83e3c5e0f5bbf712e7354ce9cb1a3b2a
saimthecan <sco11@protonmail.com>
2024-01-16 19:56:34 +0300

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
       infinite: true,
       speed: 500,
       autoplay: true,
-      autoplaySpeed: 55000,
+      autoplaySpeed: 5000,
       slidesToShow: 1,
       slidesToScroll: 1,
       swipeToSlide: true,
