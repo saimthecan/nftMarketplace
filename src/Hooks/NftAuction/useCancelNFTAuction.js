679976167380b3bb143aa099cc78a4ae346d0542
saimthecan <sco11@protonmail.com>
2024-08-13 14:46:18 +0300

    cancel auction toast update

--- src/Hooks/NftAuction/useCancelNFTAuction.js
+++ src/Hooks/NftAuction/useCancelNFTAuction.js
@@ -18,10 +18,14 @@ const useCancelNFTAuction = (signer, CONTRACT_ADDRESS) => {
       toast.success("Auction cancelled successfully!");
     } catch (error) {
       console.error("An error occurred while cancelling the auction:", error);
-      toast.error("Error cancelling auction.");
+       // Hata mesajı kontrolü ve kullanıcıya açıklayıcı toast gösterme
+       if (error.message.includes("execution reverted: Cannot cancel auction under these conditions")) {
+        toast.error("Auction can't be canceled after a bid. You can only cancel within 5 days after the auction ends.");
+      } else {
+        toast.error("Error cancelling auction. Please try again.");
+      }
     }
   };
-
   return cancelAuction;
 };
 
